c23fe0bdd863ae41d686708ef66271ba2285113f
Nick Emmerson <nickemmerson619@hotmail.com>
2021-01-12 02:59:42 -0800

    Data import function streamlined (2)

---
+++ AdventOfCSharpCode/DataProcessing.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace AdventOfCSharpCode
+{
+    public static class DataProcessing
+    {
+        private static string DataPath { get; } =
+            "C:\\Users\\nick.emmerson\\Work Unit Only\\VS Projects\\Advent of Code\\Data";
+
+        public static string[] Import(int dayNumber)
+        {
+            string path = string.Format("{0}\\Data_Day{1}.txt", DataPath, dayNumber.ToString());
+
+            if (File.Exists(path))
+            {
+                return File.ReadAllLines(path).Select(x => x.Trim()).Where(x => x.Length > 0).ToArray();
+            }
+
+            return null;
+        }
+    }
+
+}
