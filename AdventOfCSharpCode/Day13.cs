9c5cda45f2d699d398eb1f09a50f1587b9747b32
Nick Emmerson <nickemmerson619@hotmail.com>
2021-01-12 04:13:45 -0800

    Early implementation of Day 13

---
+++ AdventOfCSharpCode/Day13.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AdventOfCSharpCode
+{
+    namespace Day13
+    {
+        public class BusTimetableStorage
+        {
+            public int EarliestTimestamp { get; private set; } = 0;
+
+            public int[] Buses { get; private set; }
+
+            public BusTimetableStorage(string et, string b)
+            {
+                int et_return;
+
+                if (!int.TryParse(et, out et_return))
+                {
+                    throw new Exception("Invalid arguments");
+                }
+
+                EarliestTimestamp = et_return;
+
+                Buses = b.Split(',')
+                    .Select(x => x.Trim())
+                    .Select(x =>
+                    {
+                        int x_return;
+                        if (int.TryParse(x, out x_return))
+                        {
+                            return x_return;
+                        }
+                        return 0;
+                    })
+                    .Where(x => x > 0)
+                    .ToArray();
+            }
+        }
+
+        public class Day13
+        {
+            public static void Main(string[] args)
+            {
+
+            }
+        }
+    }
+}
---
+++ AdventOfCSharpCodeTest/Test_Day13.cs
@@ -0,0 +1,30 @@
+﻿using AdventOfCSharpCode.Day13;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AdventOfCSharpCodeTest
+{
+    namespace Day13
+    {
+        [TestFixture]
+        public static class Test_Day13
+        {
+            private static string test_timestamp = "939";
+            private static string test_buses = "7,13,x,x,59,x,31,19";
+
+            [Test]
+            public static void TestCase()
+            {
+                var test_data = new BusTimetableStorage(test_timestamp, test_buses);
+
+                Assert.That(test_data.EarliestTimestamp, Is.EqualTo(939));
+
+                Assert.That(test_data.Buses, Has.Length.EqualTo(5));
+                Assert.That(test_data.Buses, Has.All.GreaterThanOrEqualTo(7));
+                Assert.That(test_data.Buses, Has.All.LessThanOrEqualTo(59));
+            }
+        }
+    }
+}
