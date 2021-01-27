cfad9b03107042e74d054a72ea4ba31b3f499a34
Nick Emmerson <nickemmerson619@hotmail.com>
2021-01-27 02:26:22 -0800

    First steps

---
+++ AdventOfCSharpCode/DayProcessor.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AdventOfCSharpCode
+{
+
+    public interface iDayProcessor
+    {
+        string Part1 { get; }
+
+        string Part2 { get; }
+
+    }
+}
